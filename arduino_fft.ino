05e8d1fe23734391a9ba1d486903cc8c9ccf4402
Jakub Bujny <jakub.bujny@karhoo.com>
2020-01-20 17:34:17 +0100

    working

--- arduino_fft.ino
+++ arduino_fft.ino
@@ -1,10 +1,37 @@
-#include <Arduino.h>
-#include "User_Setup.h"
+#define LIN_OUT8 1 // use the log output function
+#define FFT_N 256    // set to 128 point fft
 
+#include <FFT.h> // include the library
 void setup() {
+    Serial.begin(115200); // use the serial port
+    TIMSK0 = 0; // turn off timer0 for lower jitter
+    analogRead(A5);
+    DIDR0 = 0x01; // turn off the digital input for adc0
 
 }
 
 void loop() {
-
+    cli();
+    for (int i = 0; i < FFT_N*2; i += 2) { // save 256 samples
+        while(!(ADCSRA & 0x10)); // wait for adc to be ready
+        ADCSRA = 0xf5; // restart adc
+        byte m = ADCL; // fetch adc data
+        byte j = ADCH;
+        int k = (j << 8) | m; // form into an int
+        k -= 0x0200; // form into a signed int
+        k <<= 6; // form into a 16b signed int
+        fft_input[i] = k; // put real data into even bins
+        fft_input[i + 1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_lin8(); // take the output of the fft
+    sei();
+    Serial.print("b,");
+    for(int i = 0; i < FFT_N / 2; i++) {
+        Serial.print(fft_lin_out8[i]);
+        Serial.print(",");
+    }
+    Serial.println();
 }
